69b48612dc560efefeb9f47133cec30d6a2e91ca
Cletus Kingdom <cletuskingdom@gmail.com>
2025-11-22 17:53:55 +0100

    login and registration page added

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,50 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
+
 export default function LoginPage() {
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    });
+
+    const onLogin = async () => {};
+
     return (
-        <div className="flex">
-            <h1 className="text-center text-2xl">Login Page</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-center text-2xl mb-5">LOGIN</h1>
+            <hr />
+
+            <label htmlFor="email">email</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                id="email"
+                type="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="email"
+            />
+
+            <label htmlFor="password">password</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password"
+            />
+
+            <button
+                onClick={onLogin}
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            >
+                Signup here
+            </button>
+            <Link href="/auth/register">Register here</Link>
         </div>
     );
 }
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,7 +1,61 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
+
 export default function RegisterPage() {
+    const [user, setUser] = React.useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const onSignUp = async () => {};
+
     return (
-        <div className="flex">
-            <h1 className="text-center text-2xl">Register Page</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-center text-2xl mb-5">Register Page</h1>
+            <hr />
+
+            <label htmlFor="username">Username</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                id="username"
+                type="text"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                placeholder="username"
+            />
+
+            <label htmlFor="email">email</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                id="email"
+                type="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="email"
+            />
+
+            <label htmlFor="password">password</label>
+            <input
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="password"
+            />
+
+            <button
+                onClick={onSignUp}
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+            >
+                Signup here
+            </button>
+            <Link href="/auth/login">Registered? Login here</Link>
         </div>
     );
 }
